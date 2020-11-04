a8c784b86a88648fadad8879ac62d6dc8906030c
Kinglord1090 <64801892+Kinglord1090@users.noreply.github.com>
2020-11-04 16:50:05 +0530

    Update Bruteforcer.py

--- Bruteforcer.py
+++ Bruteforcer.py
@@ -1,9 +1,8 @@
 from itertools import product
 import time
 
-def use():
-  a = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "`", "~", "@", "#", "$", "&", "*", "…", "\\", "<", ">", "'", ",", ".", "₹"]
-# A is the array which contains all the 100 characters of the keyboard!
+a = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "`", "~", "@", "#", "$", "&", "*", "…", "\\", "<", ">", "'", ",", ".", "₹"]
+# A is the array which contains all 100 characters of the keyboard!
 
 while True:
 	try:
