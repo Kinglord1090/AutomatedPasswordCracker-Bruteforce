536972af696f6d6320bc492e249b651d4296641b
Kinglord1090 <64801892+Kinglord1090@users.noreply.github.com>
2022-08-05 22:01:39 +0530

    Update Bruteforcer.py

--- Bruteforcer.py
+++ Bruteforcer.py
@@ -1,59 +1,59 @@
 from itertools import product
 import time
 
-a = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "`", "~", "@", "#", "$", "&", "*", "…", "\\", "<", ">", "'", ",", ".", "₹"]
-# A is the array which contains all 100 characters of the keyboard!
+characters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "`", "~", "@", "#", "$", "&", "*", "…", "\\", "<", ">", "'", ",", ".", "₹"]
+# characters is a list which contains 100 characters from the keyboard
 
 while True:
 	try:
-		b = int(input("Enter the minimum length of the passwords to be generated: "))
+		min_length = int(input("Enter the minimum length of the passwords to be generated: "))
 		break
 	except ValueError:
 		print("Enter a valid number!!")
-b -= 1
-# B is the minimum length of the passwords to be generated!
+min_length -= 1
+# min_length is the minimum length of the passwords to be generated
 
 while True:
 	try:
-		c = int(input("Enter the maximum length of the passwords to be generated: "))
+		max_length = int(input("Enter the maximum length of the passwords to be generated: "))
 		break
 	except ValueError:
 		print("Enter a valid number!!")
-# C is the maximum length of the passwords to be generated!
+# max_length is the maximum length of the passwords to be generated
 
-d = None            # D is the guessed password which is used later at line 42!
-e = "1234"          # E is a sample password. Change it according to your desire!
-f = time.time()     # F is the start point of time!
-g = 0               # G is the number of guesses tried out by the code!
+guess = None            # guess is the guessed combination which is used later at line 42
+password = "1234"          # password is a sample string used for testing
+start_time = time.time()     # start_time is the start point of time
+no_of_guesses = 0               # no_of_guesses is an int value thats incremented at each loop to know the number of guesses tried out by the code
 
 
 def use():
-	global b, d, e, g
-	if b < c:
-		b += 1
-		for h in product(a, repeat=b):    # H is the raw form of the guessed password(d)!
-			d = "".join(h)
-			if e != d:
-				print(d)
-				g += 1
-			elif e == d:
-				i = time.time()           # I is the stop point of time!
-				j = i - f                 # J is the total time taken!
+	if min_length < max_length:
+		min_length += 1
+		for x in product(a, repeat=b):    # x is the raw form of the combination
+			guess = "".join(x)
+			if guess != password:
+				print(guess)
+				no_of_guesses += 1
+			else:
+				stop_time = time.time()           # stop_time is the stop point of time
+				time_taken = stop_time - start_time                 # J is the total time taken!
 				try:
-					print("\nPassword Found!!\nThe password is: " + d + "\nTried out " + str(int(g)) +
-					      " possible passwords in " + str(int(j)) + " seconds!!\nThat is " + str(int(g) / int(j)) +
+					print("\nPassword Found!!\nThe password is: " + guess + "\nTried out " + str(no_of_guesses) +
+					      " possible passwords in " + str(time_taken) + " seconds!!\nThat is " + str(no_of_guesses / time_taken) +
 					      " passwords per second!!")
 					break
 				except ZeroDivisionError:
-					print("\nPassword Found!!\nThe password is: " + d + "\nTried out " + str(int(g)) +
-					      " possible passwords in " + str(int(j)) + " seconds!!\nThat is " + str(int(g)) +
+					print("\nPassword Found!!\nThe password is: " + guess + "\nTried out " + str(no_of_guesses) +
+					      " possible passwords in " + str(time_taken) + " seconds!!\nThat is " + str(no_of_guesses / time_taken) +
 					      " password guesses in less than a second!!")
 					break
-		for k in range(b):
-			if e != d:
+		for y in range(min_length):
+			if guess != password:
 				use()
-			elif e == d:
+			else:
 				break
 
 
-use()
+if __name__ == "__main__":
+	use()
